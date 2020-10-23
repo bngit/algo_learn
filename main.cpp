6a77606bcb530c16f2d6904a62e60d894fccbc2e
bngit <mathticer@gmail.com>
2020-10-23 23:16:15 +0800

    Add cmake system

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "sort/sort.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "[MAIN] I'm OK!" << endl;
+    my_sort();
+    return 0;
+}
---
+++ sort/sort.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+void my_sort()
+{
+    cout << "Hello boy!" << endl;
+}
